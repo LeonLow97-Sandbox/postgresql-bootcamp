8cbe1a2456cc6443ca2f9ea00b87850deeb72a8a
Leon Low <lowjiewei@gmail.com>
2022-12-11 21:06:42 +0800

    UPSERT query

---
+++ adnan_waheed/delete.sql
@@ -0,0 +1,15 @@
+-- DELETE RECORDS FROM A TABLE
+/*
+DELETE FROM table_name
+WHERE column_name = 'value';
+*/
+
+SELECT * FROM customers;
+
+DELETE FROM customers
+WHERE customer_id = 8;
+
+-- To delete all records
+-- DELETE FROM table_name
+DELETE FROM customers;
+
